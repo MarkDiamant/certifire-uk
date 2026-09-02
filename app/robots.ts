3e8dc1a2f7c27245dc67fbab8663b5fdc818cc67
MarkDiamant <markdiamant@gmail.com>
2026-09-02 21:48:24 +0100

    Use Certifire custom domain in robots

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://certifire-uk.vercel.app/sitemap.xml',
+    sitemap: 'https://certifireuk.co.uk/sitemap.xml',
   };
 }
